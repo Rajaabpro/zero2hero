027d6c8d59196ae7c57ed3ab4e9f7dc31190eef1
Rajaabpro <rajaab.pro@gmail.com>
2025-07-24 20:22:00 +0500

    Add color generation logic to getRandomColor function

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ function getRandomColor() {
     let red = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
+
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
 }
