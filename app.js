eaf51ef430277165aab0e2da2c369a26237d3972
Rajaabpro <rajaab.pro@gmail.com>
2025-07-24 20:20:13 +0500

    Remove commented-out code and ensure button click logs a message for color generation

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-// let smallImages = document.getElementByIdClassName('oldImage');
-
-// for (let i = 0; i < smallImages.length; i++) {
-
-//    (smallImages[i].src = "assets/spiderman_img.png");
-//    console.log(`value of i is ${i}`);
-   
-// }
+let btn = document.querySelector("button");
+btn.addEventListener("click", function() {
+    console.log("genrate a random color");
+    
+});
