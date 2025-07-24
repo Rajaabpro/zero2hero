92286a25ba4a5d6a0e4139c9f4c08eaa856fe344
Rajaabpro <rajaab.pro@gmail.com>
2025-07-24 20:21:23 +0500

    Add getRandomColor function to generate random RGB values

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ btn.addEventListener("click", function() {
     console.log("genrate a random color");
     
 });
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+}
