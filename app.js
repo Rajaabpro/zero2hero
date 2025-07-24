a794219dca2bdeef59a7b64e894d0184633b56d5
Rajaabpro <rajaab.pro@gmail.com>
2025-07-24 20:13:48 +0500

    Remove commented-out code for smallImages processing in app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// let smallImages = document.getElementByIdClassName('oldImage');
+
+// for (let i = 0; i < smallImages.length; i++) {
+
+//    (smallImages[i].src = "assets/spiderman_img.png");
+//    console.log(`value of i is ${i}`);
+   
+// }
